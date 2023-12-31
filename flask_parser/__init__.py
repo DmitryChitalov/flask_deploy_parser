8b0c97d47eccde2f7fd8b760375a8bda38b21090
DmitryChitalov <cdi9@yandex.ru>
2023-12-31 10:56:32 +0500

    !

--- flask_parser/__init__.py
+++ flask_parser/__init__.py
@@ -4,4 +4,8 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
+    from flask_parser.flask_parser.flask_parser import parser_blueprint
+
+    app.register_blueprint(parser_blueprint)
+
     return app
