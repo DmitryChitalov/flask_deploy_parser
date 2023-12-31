4647b6f7deb25e400619d0f0aa8ca669292c898f
DmitryChitalov <cdi9@yandex.ru>
2023-12-31 11:12:04 +0500

    !

--- flask_parser/__init__.py
+++ flask_parser/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
 
-
+SECRET_KEY = b'\x143#\x1eV;\xc9\xa0\xecr\r\xd4/{b\n'
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(__name__)
+
 
     from flask_parser.flask_parser.flask_parser import parser_blueprint
+    from flask_parser.authorization.auth import auth_blueprint
 
     app.register_blueprint(parser_blueprint)
+    app.register_blueprint(auth_blueprint)
 
     return app
--- hh_flask_parser.py
+++ hh_flask_parser.py
@@ -3,4 +3,5 @@ from flask_parser import create_app
 app = create_app()
 
 if __name__ == '__main__':
+
     app.run(debug=True)
