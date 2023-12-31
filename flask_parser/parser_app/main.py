9aa5ca731fec51507f3a14a4f113c7237b6e8c2e
DmitryChitalov <cdi9@yandex.ru>
2023-12-31 19:43:04 +0500

    !

--- flask_parser/parser_app/main.py
+++ flask_parser/parser_app/main.py
@@ -23,25 +23,25 @@ def main():
 
     i_cycle = 0
     while True:
-
-        # Читаем записи со статусом 0 из БД
-        rows = read_requests(db_session)
-        print(f" я тут {rows}")
-        if rows:
-            # Если записи найдены, то начинаем обработку
-            for row_request in rows:
-                print(
-                    f"\nОбработка запроса: {row_request.region} {row_request.text_request} начата.")
-                process_request(db_session, file_folder, row_request)
-                print(
-                    f"Обработка запроса: {row_request.region} {row_request.text_request} завершена.")
-
-        else:
-            ## Переходим в режим ожидания
-            time.sleep(5)
-            sys.stdout.write("\r")
-            sys.stdout.write(f"Новых запросов не найдено. Цикл {i_cycle}")
-        i_cycle += 1
+        print("хуй")
+        # # Читаем записи со статусом 0 из БД
+        # rows = read_requests(db_session)
+        # print(f" я тут {rows}")
+        # if rows:
+        #     # Если записи найдены, то начинаем обработку
+        #     for row_request in rows:
+        #         print(
+        #             f"\nОбработка запроса: {row_request.region} {row_request.text_request} начата.")
+        #         process_request(db_session, file_folder, row_request)
+        #         print(
+        #             f"Обработка запроса: {row_request.region} {row_request.text_request} завершена.")
+        #
+        # else:
+        #     ## Переходим в режим ожидания
+        #     time.sleep(5)
+        #     sys.stdout.write("\r")
+        #     sys.stdout.write(f"Новых запросов не найдено. Цикл {i_cycle}")
+        # i_cycle += 1
 
 
 if __name__ == "__main__":
