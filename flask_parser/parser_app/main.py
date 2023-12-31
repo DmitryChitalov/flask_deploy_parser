3aaf7b4ac64ec0ab2027a36c65662fddc8107add
DmitryChitalov <cdi9@yandex.ru>
2023-12-31 20:32:26 +0500

    !

--- flask_parser/parser_app/main.py
+++ flask_parser/parser_app/main.py
@@ -5,49 +5,50 @@ import sys
 import os
 import unittest
 import json
+
 sys.path.append(os.path.join(os.getcwd(), '..'))
 import sys
 import os
 import unittest
 import json
-#sys.path.append(os.path.join(os.getcwd(), '..'))
-#print(1, os.path.join(os.getcwd()))
+# sys.path.append(os.path.join(os.getcwd(), '..'))
+# print(1, os.path.join(os.getcwd()))
 
 from .process_request import read_requests, process_request
 from .database import db_session
 
-#print("хуец")
-#def main():
 
-# Читаем конфигурационные параметры
-config = cfg.ConfigParser()
-config.read("./flask_parser/hh_config.ini")
-sqlite_db = config["SQLite"]["path"]
-file_folder = config["Json"]["path"]
+# print("хуец")
+def main():
+    # print("хуец")
+    # Читаем конфигурационные параметры
+    config = cfg.ConfigParser()
+    config.read("./flask_parser/hh_config.ini")
+    sqlite_db = config["SQLite"]["path"]
+    file_folder = config["Json"]["path"]
 
-i_cycle = 0
-while True:
-    print("хай")
-    # Читаем записи со статусом 0 из БД
-    rows = read_requests(db_session)
-    # print(f" я тут {rows}")
-    if rows:
-    #Если записи найдены, то начинаем обработку
-        for row_request in rows:
-            print(
-                f"\nОбработка запроса: {row_request.region} {row_request.text_request} начата.")
-            process_request(db_session, file_folder, row_request)
-            print(
-                f"Обработка запроса: {row_request.region} {row_request.text_request} завершена.")
+    i_cycle = 0
+    while True:
+        pass
+        # Читаем записи со статусом 0 из БД
+        rows = read_requests(db_session)
+        print(f" я тут {rows}")
+        if rows:
+            # Если записи найдены, то начинаем обработку
+            for row_request in rows:
+                print(
+                    f"\nОбработка запроса: {row_request.region} {row_request.text_request} начата.")
+                process_request(db_session, file_folder, row_request)
+                print(
+                    f"Обработка запроса: {row_request.region} {row_request.text_request} завершена.")
 
-    else:
-        # Переходим в режим ожидания
-        time.sleep(2)
-        sys.stdout.write("\r")
-        sys.stdout.write(f"Новых запросов не найдено. Цикл {i_cycle}")
-    i_cycle += 1
+        else:
+            ## Переходим в режим ожидания
+            time.sleep(5)
+            sys.stdout.write("\r")
+            sys.stdout.write(f"Новых запросов не найдено. Цикл {i_cycle}")
+        i_cycle += 1
 
+# if __name__ == "__main__":
 
-#if __name__ == "__main__":
-
-    #main()
+# main()
--- hh_flask_parser.py
+++ hh_flask_parser.py
@@ -1,11 +1,13 @@
 from flask_parser import create_app
 import multiprocessing as ml
 from flask_parser.parser_app.main import main
+
 app = create_app()
 
 if __name__ == '__main__':
 
-    #par_service = ml.Process(name="HH Parser", target=pr.main)
-    #par_service.start()
-    #main()
+    par_service = ml.Process(name="HH Parser", target=main)
+    par_service.start()
+
+
     app.run(debug=True)
