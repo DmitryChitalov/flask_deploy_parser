a612f35219653e91b6c82b1cd7f3e66d2ab021f0
DmitryChitalov <cdi9@yandex.ru>
2023-12-31 20:07:46 +0500

    !

--- flask_parser/parser_app/main.py
+++ flask_parser/parser_app/main.py
@@ -18,7 +18,7 @@ from .database import db_session
 
 #print("хуец")
 #def main():
-print("хуец")
+
 # Читаем конфигурационные параметры
 config = cfg.ConfigParser()
 config.read("./flask_parser/hh_config.ini")
@@ -27,25 +27,25 @@ file_folder = config["Json"]["path"]
 
 i_cycle = 0
 while True:
-    print("хуй")
-        # # Читаем записи со статусом 0 из БД
-        # rows = read_requests(db_session)
-        # print(f" я тут {rows}")
-        # if rows:
-        #     # Если записи найдены, то начинаем обработку
-        #     for row_request in rows:
-        #         print(
-        #             f"\nОбработка запроса: {row_request.region} {row_request.text_request} начата.")
-        #         process_request(db_session, file_folder, row_request)
-        #         print(
-        #             f"Обработка запроса: {row_request.region} {row_request.text_request} завершена.")
-        #
-        # else:
-        #     ## Переходим в режим ожидания
-        #     time.sleep(5)
-        #     sys.stdout.write("\r")
-        #     sys.stdout.write(f"Новых запросов не найдено. Цикл {i_cycle}")
-        # i_cycle += 1
+
+    # Читаем записи со статусом 0 из БД
+    rows = read_requests(db_session)
+    # print(f" я тут {rows}")
+    if rows:
+    #Если записи найдены, то начинаем обработку
+        for row_request in rows:
+            print(
+                f"\nОбработка запроса: {row_request.region} {row_request.text_request} начата.")
+            process_request(db_session, file_folder, row_request)
+            print(
+                f"Обработка запроса: {row_request.region} {row_request.text_request} завершена.")
+
+    else:
+        # Переходим в режим ожидания
+        time.sleep(5)
+        sys.stdout.write("\r")
+        sys.stdout.write(f"Новых запросов не найдено. Цикл {i_cycle}")
+    i_cycle += 1
 
 
 #if __name__ == "__main__":
