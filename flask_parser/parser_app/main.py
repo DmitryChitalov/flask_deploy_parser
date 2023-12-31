35eda9b516a8f8c649aaad9b9c62d1f20f354bb4
DmitryChitalov <cdi9@yandex.ru>
2023-12-31 19:55:14 +0500

    !

--- flask_parser/parser_app/main.py
+++ flask_parser/parser_app/main.py
@@ -18,7 +18,7 @@ from .database import db_session
 
 
 def main():
-    print("хуй")
+    print("хуец")
     # Читаем конфигурационные параметры
     config = cfg.ConfigParser()
     config.read("./flask_parser/hh_config.ini")
